79c113f80532cc80d68409cfe49b09b60b5c89e6
Eugene <jsolomko@bk.ru>
2024-12-18 17:07:48 +0300

    -

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
+      theme: ThemeData(fontFamily: 'Raleway',
         scaffoldBackgroundColor: Colors.white,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         primaryColor: Colors.white,
